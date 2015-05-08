7051cf290c447e29a63a79666b48097d6ee9f512
Phoenix Nemo <i@phoenixlzx.com>
2015-05-08 11:54:36 +0800

    add basic add&query functions

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,109 @@ var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
 
+var config = require('../config');
+
 /* GET home page. */
 router.get('/', function (req, res) {
     // redirect to index.html to use angular.js
     res.redirect('/index.html');
+});
+
+router.post('/add', function (req, res) {
+
+    var time = req.body.time + '';
+    var station = req.body.station + '';
+    var report = req.body.report + '';
+
+    MongoClient.connect(config.mongodb, {
+        db: {
+            native_parser: true,
+            w: 1
+        }
+    }, function (err, db) {
+        if (err) {
+            throw err;
+        }
+
+        var collection = db.collection('reports');
+
+        // TODO integrity check
+
+        collection.insertOne({
+            time: time,
+            station: station,
+            report: report
+        }, function(err) {
+            if (err) {
+                res.send(500);
+            } else {
+                res.send(200);
+            }
+        });
+
+    });
+});
+
+router.get('/recent', function(req, res) {
+
+    MongoClient.connect(config.mongodb, {
+        db: {
+            native_parser: true,
+            w: 1
+        }
+    }, function (err, db) {
+        if (err) {
+            throw err;
+        }
+
+        var collection = db.collection('reports');
+
+        collection.find().sort({
+            time: -1
+        }).limit(10).toArray(function(err, reports) {
+            if (err) {
+                res.send(500);
+            } else {
+                res.send(200, reports);
+            }
+        });
+
+    });
+
+});
+
+router.get('/q/:time/:station', function(req, res) {
+
+    var time = req.params.time + '';
+    var station = req.params.station + '';
+
+    MongoClient.connect(config.mongodb, {
+        db: {
+            native_parser: true,
+            w: 1
+        }
+    }, function (err, db) {
+        if (err) {
+            throw err;
+        }
+
+        var collection = db.collection('reports');
+
+        // TODO integrity check
+
+        collection.findOne({
+            time: time,
+            station: station
+        }, function(err, report) {
+            if (err) {
+                res.send(500);
+            } else {
+                res.send(200, report);
+            }
+        });
+
+    });
+
 });
 
 module.exports = router;
