06e3664d333f340c534cbd189dfd41cda8ba27ab
Phoenix Nemo <i@phoenixlzx.com>
2015-06-09 11:06:01 +0800

    add test data

---
+++ test/synop_test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var fs = require('fs');
+var synop = require('../lib/synop');
+
+switch (process.argv[2]) {
+    case 'decode':
+        synop.decode(process.argv[3], function(err, decoded) {
+            console.log('Decoded: ' + decoded);
+        });
+        break;
+    case 'query':
+        var q = process.argv[3]; // AAXX0724
+        var t = process.argv[4]; // T00
+        fs.readFile('./data/' + q + '.T' + t, 'utf8', function(err, data) {
+            if (err) {
+                console.error('Error: ' + err);
+                process.exit(1);
+            }
+            var d = data.split('\n');
+            synop.decode(d[3], function(err, decoded) {
+                console.log(decoded);
+            });
+
+        });
+        break;
+    default:
+        console.log('Usage: decode "..." | query [STA] [TIME]');
+}
